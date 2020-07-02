9d206860208a4d02eb49e3645a48bd8215e51980
takumi_futatsugi <takumi_futatsugi@sobal.co.jp>
2020-07-02 15:47:14 +0900

    プロジェクトを作成。AndroidStudioからのコミットテスト

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button  =   findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
+        rollButton.setOnClickListener{ rollDice() }
+    }
+
+    private fun rollDice(){
+        //Toast.makeText(this, "Button Clicked",Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+
+}
